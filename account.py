4911f65666139852b8bbcc31a82c10d0f63824cb
Jacob White <79297478+Ruhevoll@users.noreply.github.com>
2022-11-18 19:01:58 -0600

    Step 8

--- account.py
+++ account.py
@@ -3,27 +3,54 @@
 
 class Account:
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
+        """
+        Function to create a bank account.
+
+        :param name: Name of the bank account.
+        """
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        Function to deposit funds into a bank account.
+
+        :param amount: Amount to be deposited.
+        :return: Whether deposit was successful.
+        """
         if amount > 0:
             self.__account_balance = self.__account_balance + amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        Function to withdraw funds from a bank account.
+
+        :param amount: Amount to be withdrawn.
+        :return: Whether withdrawal was successful.
+        """
         if (amount <= self.__account_balance) and (amount > 0):
             self.__account_balance = self.__account_balance - amount
             return True
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Get the balance of a bank account.
+
+        :return: The balance of the bank account.
+        """
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Get the name of a bank account.
+
+        :return: The name of the bank account.
+        """
         return self.__account_name
 
---
+++ test_account.py
@@ -0,0 +1,59 @@
+import pytest
+from account import *
+
+class Test:
+    def setup_method(self):
+        self.p1 = Account('Jane')
+        self.p2 = Account('John')
+
+    def teardown_method(self):
+        del self.p1
+        del self.p2
+
+    def test_init(self):
+        assert self.p1.get_name() == 'Jane'
+        assert self.p1.get_balance() == 0
+
+        assert self.p2.get_name() == 'John'
+        assert self.p2.get_balance() == 0
+
+    def test_deposit(self):
+        # Basic deposit test
+        assert self.p1.deposit(30) is True
+        assert self.p1.get_balance() == 30
+
+        # Zero deposit test
+        assert self.p1.deposit(0) is False
+        assert self.p1.get_balance() == 30
+
+        # Negative deposit test
+        assert self.p1.deposit(-30) is False
+        assert self.p1.get_balance() == 30
+
+    def test_withdraw(self):
+        self.p1.deposit(30)
+
+        # Basic withdraw test
+        assert self.p1.withdraw(25) is True
+        assert self.p1.get_balance() == 5
+
+        # Withdrawing beyond current account balance
+        assert self.p1.withdraw(10) is False
+        assert self.p1.get_balance() == 5
+
+        # Zero withdraw test
+        assert self.p1.withdraw(0) is False
+        assert self.p1.get_balance() == 5
+
+        # Negative withdraw test
+        assert self.p1.withdraw(-10) is False
+        assert self.p1.get_balance() == 5
+
+    def test_get_balance(self):
+        assert self.p1.get_balance() == 0
+        self.p1.deposit(25.34)
+        assert self.p1.get_balance() == 25.34
+
+    def test_get_name(self):
+        assert self.p1.get_name() == 'Jane'
+        assert self.p2.get_name() == 'John'
